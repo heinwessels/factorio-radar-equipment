ee931e1cf0e40741c87dc74d085cc8dda815d6cf
Hein Wessels <heinwessels93@gmail.com>
2025-06-10 21:06:39 +0200

    Added compatibility with Krastorio 2 vehicles

--- data.lua
+++ data.lua
@@ -1,2 +1,4 @@
 require("prototypes.radar-equipment")
 require("prototypes.hidden-radar")
+
+require("prototypes.compatibility.krastorio2")
---
+++ prototypes/compatibility/krastorio2.lua
@@ -0,0 +1,3 @@
+if not mods["Krastorio2"] then return end
+
+table.insert(data.raw["night-vision-equipment"]["radar-equipment"].categories, "kr-vehicle")
