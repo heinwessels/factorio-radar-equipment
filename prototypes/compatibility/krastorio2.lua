76a8d8825259f94e7eaef39a42d6d1fdca129709
Hein Wessels <heinwessels93@gmail.com>
2025-06-30 22:02:45 +0200

    Added compatibility with Krastorio 2 Spaced Out vehicles.

--- prototypes/compatibility/krastorio2.lua
+++ prototypes/compatibility/krastorio2.lua
@@ -1,3 +1,3 @@
-if not mods["Krastorio2"] then return end
+if not (mods["Krastorio2"] or mods["Krastorio2-spaced-out"]) then return end
 
 table.insert(data.raw["night-vision-equipment"]["radar-equipment"].categories, "kr-vehicle")
